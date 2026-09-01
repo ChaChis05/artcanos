d6d064df5b14275240907a02a4e90ef9e3abea5b
ChaChis05 <zaidycanobenavides@gmail.com>
2026-09-01 17:31:17 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,52 +1,61 @@
+function inicializarAdministrador(){
+
+    let usuarios =
+        JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    if(usuarios.length === 0){
+
+        usuarios = [
+            {
+                nombre: "Administrador",
+                usuario: "admin",
+                clave: "1234",
+                rol: "Administrador",
+                categorias: []
+            }
+        ];
+
+        localStorage.setItem(
+            "usuarios",
+            JSON.stringify(usuarios)
+        );
+    }
+}
+
+
 function login(){
 
+    inicializarAdministrador();
 
-let usuario =
-document.getElementById("usuario").value;
+    let usuario =
+        document.getElementById("usuario").value;
 
+    let clave =
+        document.getElementById("clave").value;
 
-let clave =
-document.getElementById("clave").value;
+    let usuarios =
+        JSON.parse(localStorage.getItem("usuarios")) || [];
 
+    let encontrado = usuarios.find(u =>
+        u.usuario === usuario &&
+        u.clave === clave
+    );
 
+    if(encontrado){
 
-let usuarios =
-JSON.parse(localStorage.getItem("usuarios")) || [];
+        localStorage.setItem(
+            "usuarioActivo",
+            JSON.stringify(encontrado)
+        );
 
+        window.location.href = "dashboard.html";
 
+    }
 
-let encontrado = usuarios.find(u =>
+    else{
 
-u.usuario === usuario &&
-u.clave === clave
+        document.getElementById("mensaje").innerHTML =
+            "❌ Usuario o contraseña incorrectos";
 
-);
-
-
-
-if(encontrado){
-
-
-localStorage.setItem(
-"usuarioActivo",
-JSON.stringify(encontrado)
-);
-
-
-
-window.location.href="dashboard.html";
-
-
-}
-
-else{
-
-
-document.getElementById("mensaje").innerHTML =
-"❌ Usuario o contraseña incorrectos";
-
-
-}
-
-
+    }
 }
